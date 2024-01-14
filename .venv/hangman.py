4c5f2f35cccf24d16176b9c71693825714f176af
rodrigofdl <rodrigo.fdlira@gmail.com>
2024-01-14 00:52:07 -0300

    Sixth commit - I added the main function of the game

--- .venv/hangman.py
+++ .venv/hangman.py
@@ -31,3 +31,39 @@ def exibir_tentativas_restantes(tentativas):
 # Função para receber a letra do jogador
 def receber_letra():
     return input("Digite uma letra: ").lower()
+
+# Função principal do jogo
+def jogo_da_forca():
+    limpa_tela()
+    palavra_secreta = palavra_aleatoria()
+    letras_corretas = []
+    tentativas_restantes = 6
+
+    while tentativas_restantes > 0:
+        palavra_oculta = exibir_palavra_oculta(palavra_secreta, letras_corretas)
+        print(palavra_oculta)
+        exibir_tentativas_restantes(tentativas_restantes)
+        letra_digitada = receber_letra()
+
+        if letra_digitada in letras_corretas:
+            print("Essa letra já foi tentada! Tente novamente.")
+            continue
+
+        if letra_digitada in palavra_secreta:
+            letras_corretas.append(letra_digitada)
+            if all(letra in letras_corretas for letra in palavra_secreta):
+                print("Parabens! Você venceu. A palavra é:", palavra_secreta)
+                break
+
+        if letra_digitada == palavra_secreta:
+            print("Parabens! Você venceu. A palavra é:", palavra_secreta)
+            break
+
+        else:
+            tentativas_restantes -= 1
+
+        if tentativas_restantes == 0:
+            print("Você perdeu! A palavra correta era:", palavra_secreta)
+
+# Iniciar jogo
+jogo_da_forca()
